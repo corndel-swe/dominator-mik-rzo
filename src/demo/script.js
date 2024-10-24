73d47d5e374ee81608474c559931bba15cf5eb0f
mik-rzo <michael.panong3@gmail.com>
2024-10-24 15:53:47 +0100

    completed demo

--- src/demo/script.js
+++ src/demo/script.js
@@ -1,7 +1,17 @@
-// TODO: get a reference to the .box element and log it to the console
+// get a reference to the .box element and log it to the console
+const box = document.getElementsByClassName("box")[0];
 
-// TODO: add an event listener to the .box element that toggles the .left class
+// add an event listener to the .box element that toggles the .left class
+box.addEventListener("click", () => {
+  box.classList.toggle("left");
+})
 
-// TODO: get a reference to #button, #txt and #output
+// get a reference to #button, #txt and #output
+const button = document.getElementById("button");
+const inputText = document.getElementById("txt");
+const outputText = document.getElementById("output");
 
-// TODO: add an event listener to the #button that changes the text in #output to the lowercase value of #txt
+// add an event listener to the #button that changes the text in #output to the lowercase value of #txt
+button.addEventListener("click", () => {
+  outputText.textContent = inputText.value.toLowerCase();
+})
