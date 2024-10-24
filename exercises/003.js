83f08b2afc574cf6fdbb6a8e32d8569de392f5e9
mik-rzo <michael.panong3@gmail.com>
2024-10-24 10:53:54 +0100

    removed TODO from comments

--- exercises/003.js
+++ exercises/003.js
@@ -1,10 +1,10 @@
-// TODO: https://tech-docs.corndel.com/js/data-types.html
+// https://tech-docs.corndel.com/js/data-types.html
 
-// TODO: Replace null with the string title of your favourite book
+// Replace null with the string title of your favourite book
 export const title = 'Catching Fire'
 
-// TODO: Replace null with the integer year of its release date
+// Replace null with the integer year of its release date
 export const year = 2009
 
-// TODO: Replace null with the boolean value of whether it was released before 2000
+// Replace null with the boolean value of whether it was released before 2000
 export const isOld = false
